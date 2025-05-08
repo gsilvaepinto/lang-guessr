2c6c90b9edab21a3ca1125c1651909bf84f344f4
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-05-09 00:08:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+import { franc, francAll } from "franc";
+import colors from 'colors';
+import langs from 'langs';
+
+function main(){
+    const weakLangs = {
+        sco: 'Scotish'
+    };
+
+    const input = process.argv[2];
+
+    if (!input){
+        console.log("Please provide a text input.".red);
+        return;
+    }
+
+    const topGuesses = francAll(input);
+
+    if (!topGuesses.length || topGuesses[0][0] === 'und'){
+        console.log("Sorry, couldn't determine the language.".yellow);
+        return;
+    }
+
+    console.log("Top language guesses:\n".cyan);
+    topGuesses.slice(0, 3).forEach(([code, score], index) => {
+        const lang = langs.where('3', code);
+        const name = lang ? lang.name : weakLangs[code] || code;
+        const line = `${index + 1}. ${name} (${code}) – score: ${score.toFixed(4)}`;
+        console.log(line);
+    })
+
+    const [bestGuess] = topGuesses[0];
+    const bestLang = langs.where('3', bestGuess) || {name: weakLangs[bestGuess] || bestGuess}
+
+    console.log(`\nDetected language: ${bestLang.name}`.green);
+}
+
+main();
